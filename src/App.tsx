623dee928159c132821ecbe3c822ab7f9bc26eb8
qpounodb <qpundb+github@ya.ru>
2022-08-23 20:52:58 +0300

    chore: add react-router and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 export const App: React.FC = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="about" element={<div>About</div>} />
+      </Routes>
+    </div>
+  );
 };
