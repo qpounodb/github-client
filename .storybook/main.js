6fa17026ea72277b12b35a58b4e67767d8f08da1
qpounodb <qpundb+github@ya.ru>
2022-08-25 21:26:18 +0300

    chore: update storybook options

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
