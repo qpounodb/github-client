fffe783ded307ae61a4f092a200be4267585830b
qpounodb <qpundb+github@ya.ru>
2022-08-26 11:07:58 +0300

    refactor: move onSubmit to input props

--- src/App/components/Input/Input.tsx
+++ src/App/components/Input/Input.tsx
@@ -8,12 +8,30 @@ type InputChangeHandler = React.ChangeEventHandler<HTMLInputElement>;
 export type InputProps = Omit<InputHTMLProps, 'value' | 'onChange'> & {
   value: string;
   onChange: (value: string) => void;
+  onSubmit?: (value: string) => void;
 };
 
-export const Input: React.FC<InputProps> = ({ onChange, ...rest }) => {
+export const Input: React.FC<InputProps> = ({
+  onChange,
+  onSubmit,
+  ...rest
+}) => {
   const cls = classname(styles.input, rest.className);
 
   const handler: InputChangeHandler = (e) => onChange(e.target.value);
 
-  return <input {...rest} className={cls} type="text" onChange={handler} />;
+  const handleEnter: React.KeyboardEventHandler = (e) => {
+    if (e.key !== 'Enter') return;
+    onSubmit?.(rest.value);
+  };
+
+  return (
+    <input
+      {...rest}
+      className={cls}
+      type="text"
+      onChange={handler}
+      onKeyDown={handleEnter}
+    />
+  );
 };
--- src/App/components/Search/Search.tsx
+++ src/App/components/Search/Search.tsx
@@ -20,19 +20,14 @@ export const Search: React.FC<SearchProps> = ({
 }) => {
   const handleSubmit = () => onSubmit(value);
 
-  const handleEnter: React.KeyboardEventHandler = (e) => {
-    if (e.key !== 'Enter') return;
-    handleSubmit();
-  };
-
   return (
     <div className={styles.main}>
       <Input
         placeholder={placeholder}
         value={value}
-        onChange={onChange}
         disabled={loading}
-        onKeyDown={handleEnter}
+        onChange={onChange}
+        onSubmit={handleSubmit}
       />
       <SearchButton onClick={handleSubmit} loading={loading} />
     </div>
