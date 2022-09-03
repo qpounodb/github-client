3e9003fe8ffa84370e91f46100f72af5571bbfdc
qpounodb <qpundb+github@ya.ru>
2022-09-03 12:10:27 +0300

    reafactor: optimize inner calculation

--- src/App/pages/Repo/components/RepoLangs/Lang/Lang.tsx
+++ src/App/pages/Repo/components/RepoLangs/Lang/Lang.tsx
@@ -8,8 +8,15 @@ export type LangPrpos = {
   total: number;
 };
 
+const getFraction = (count: number, total: number): number =>
+  Math.round((count / total) * 10000) / 100;
+
 export const Lang: React.FC<LangPrpos> = ({ lang, count, total }) => {
-  const fraction = Math.round((count / total) * 10000) / 100;
+  const fraction = React.useMemo(
+    () => getFraction(count, total),
+    [count, total]
+  );
+
   return (
     <div className={styles.root}>
       <img src={getLangLogo(lang)} alt={lang} />
