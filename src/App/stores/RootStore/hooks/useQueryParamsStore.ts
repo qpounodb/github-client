a7541de83ed7c5633c42b47e9d958691e036e282
qpounodb <qpundb+github@ya.ru>
2022-09-09 20:56:48 +0300

    refactor: optimize setters calls

--- src/App/stores/RootStore/hooks/useQueryParamsStore.ts
+++ src/App/stores/RootStore/hooks/useQueryParamsStore.ts
@@ -1,9 +1,15 @@
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { rootStore } from '../instance';
 
 export const useQueryParamsStore = (): void => {
   const [params, setParams] = useSearchParams();
 
-  rootStore.queryParamsStore.setSearchParamsSetter(setParams);
-  rootStore.queryParamsStore.setParams(params);
+  React.useEffect(() => {
+    rootStore.queryParamsStore.setParams(params);
+  }, [params]);
+
+  React.useEffect(() => {
+    rootStore.queryParamsStore.setSearchParamsSetter(setParams);
+  }, [setParams]);
 };
