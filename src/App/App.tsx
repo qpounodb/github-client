1ee47e2388399bc73895df9e606ea82a359b60cd
qpounodb <qpundb+github@ya.ru>
2022-09-10 21:02:51 +0300

    feat: connect notify store to app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,20 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
+import { NotifyView } from './components/NotifyView';
 import { Main } from './pages/Main';
 import { Repo } from './pages/Repo';
+import { rootStore } from './stores/RootStore';
 import { useQueryParamsStore } from './stores/RootStore/hooks/useQueryParamsStore';
 
 const App: React.FC = () => {
   useQueryParamsStore();
+
+  const handleMessageClose = React.useCallback(
+    (id: number) => rootStore.notifyStore.remove(id),
+    []
+  );
 
   return (
     <div className={styles.app}>
@@ -14,8 +23,12 @@ const App: React.FC = () => {
         <Route path="repo/:orgName/:repoName" element={<Repo />} />
         <Route path="*" element={<h1>There's nothing here!</h1>} />
       </Routes>
+      <NotifyView
+        messages={rootStore.notifyStore.messages}
+        onClose={handleMessageClose}
+      />
     </div>
   );
 };
 
-export default App;
+export default observer(App);
