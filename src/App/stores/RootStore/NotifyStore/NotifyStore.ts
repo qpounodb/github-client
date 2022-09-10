01ea42e7508b388525470165988a786cd08b2e9f
qpounodb <qpundb+github@ya.ru>
2022-09-10 21:02:21 +0300

    feat: add auto removing messages reaction

--- src/App/stores/RootStore/NotifyStore/NotifyStore.ts
+++ src/App/stores/RootStore/NotifyStore/NotifyStore.ts
@@ -1,40 +1,72 @@
-import { action, computed, makeObservable, observable } from 'mobx';
+import {
+  action,
+  computed,
+  IReactionDisposer,
+  makeObservable,
+  observable,
+  reaction,
+  runInAction,
+} from 'mobx';
 import { toError } from '~/shared/utils';
 import { Level } from './Level';
 import { Message } from './Message';
 
-type PrivateFields = '_queue' | '_enqueue' | '_dequeue';
+type PrivateFields =
+  | '_messages'
+  | '_removing'
+  | '_enqueue'
+  | '_dequeue'
+  | '_needDequeue';
+
+export type NotifyStoreConfig = {
+  maxItems?: number;
+  autoRemoveTimeout?: number;
+};
 
 export class NotifyStore {
   private _id: number = 0;
-  private _queue: Message[] = [];
+  private _messages: Message[] = [];
+  private _removing: boolean = false;
+  private _config: Required<NotifyStoreConfig>;
 
-  constructor() {
+  constructor({ maxItems = 0, autoRemoveTimeout = 5 }: NotifyStoreConfig = {}) {
+    this._config = { maxItems, autoRemoveTimeout };
+
     makeObservable<NotifyStore, PrivateFields>(this, {
-      _queue: observable,
-      isEmpty: computed,
-      getNext: action,
-      _enqueue: action,
-      _dequeue: action,
+      _messages: observable.ref,
+      _removing: observable,
+      messages: computed,
+      _needDequeue: computed,
+      remove: action.bound,
+      _enqueue: action.bound,
+      _dequeue: action.bound,
     });
+
+    this._init();
   }
 
-  get isEmpty(): boolean {
-    return this._queue.length === 0;
+  get messages(): Message[] {
+    return this._messages.slice();
   }
 
-  getNext(): Message | null {
-    if (this.isEmpty) return null;
-    return this._dequeue();
+  private get _needDequeue(): boolean {
+    return !this._removing && this._messages.length > this._config.maxItems;
   }
 
-  _enqueue(level: Level, text: string): void {
-    this._queue.push({ id: this._id, level, text, time: new Date() });
+  private _enqueue(level: Level, text: string): void {
+    const message: Message = { id: this._id, level, text, time: new Date() };
+    this._messages = [...this.messages, message];
     this._id += 1;
   }
 
-  _dequeue(): Message | null {
-    return this._queue.shift() ?? null;
+  private _dequeue(): Message {
+    const [message, ...rest] = this._messages;
+    this._messages = rest;
+    return message;
+  }
+
+  remove(id: number): void {
+    this._messages = this._messages.filter((msg) => msg.id !== id);
   }
 
   error(err: unknown): void {
@@ -48,5 +80,25 @@ export class NotifyStore {
 
   info(text: string): void {
     this._enqueue(Level.info, text);
+  }
+
+  private _overflowReaction?: IReactionDisposer;
+
+  _init(): void {
+    this._overflowReaction = reaction(
+      () => this._needDequeue,
+      (needRemove) => {
+        if (!needRemove) return;
+        this._removing = true;
+        setTimeout(
+          () =>
+            runInAction(() => {
+              this._dequeue();
+              this._removing = false;
+            }),
+          1000 * this._config.autoRemoveTimeout
+        );
+      }
+    );
   }
 }
