8f50c8b56d341e172660b07e61887683c8dc9cce
qpounodb <qpundb+github@ya.ru>
2022-09-19 22:16:13 +0300

    fix: pass wrong props

--- src/App/components/Footer/Footer.tsx
+++ src/App/components/Footer/Footer.tsx
@@ -9,6 +9,9 @@ import { config } from './config';
 import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
+  const { Logo: SchoolLogo, ...schoolProps } = config.school;
+  const { Logo: StudentLogo, ...studentProps } = config.student;
+
   return (
     <Block className={styles.block}>
       <footer className={styles.root}>
@@ -16,16 +19,15 @@ const Footer: React.FC = () => {
           <time dateTime="2022-09-01">09.2022</time>
         </div>
         <div className={styles.root__item}>
-          <ExternalLink {...config.school} className={styles.root__link}>
-            <config.school.Logo
+          <ExternalLink {...schoolProps} className={styles.root__link}>
+            <SchoolLogo
               className={join(styles.root__logo, styles.root__logo_school)}
             />
           </ExternalLink>
         </div>
         <div className={styles.root__item}>
-          <ExternalLink {...config.student} className={styles.root__link}>
-            <config.student.Logo className={styles.root__logo} />{' '}
-            {config.student.text}
+          <ExternalLink {...studentProps} className={styles.root__link}>
+            <StudentLogo className={styles.root__logo} /> {studentProps.text}
           </ExternalLink>
         </div>
       </footer>
